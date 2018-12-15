52fbc93b88784bf93e5813a66dec6a493d6645e3
Hannah Holzmann <hholzmann16@gmail.com>
2018-12-15 01:28:21 -0800

    working on finishing post request with error and success handling

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,13 +1,18 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 
+import { ContactForm } from "./contact/contactForm";
+
 @Injectable({
   providedIn: "root"
 })
 export class ApiService {
   constructor(private http: HttpClient) {}
 
-  postContact() {
-    return this.http.post;
+  postContact(contactForm: ContactForm) {
+    return this.http.post(
+      "http://interview-contact-submit-api-lb-1009699934.us-east-1.elb.amazonaws.com/contact-us/send",
+      contactForm
+    );
   }
 }
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from "@angular/core";
 import { ApiService } from "../api.service";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
+import { Observable } from "rxjs";
+import { catchError } from "rxjs/operators";
+
 @Component({
   selector: "app-contact",
   templateUrl: "./contact.component.html",
@@ -9,25 +12,33 @@ import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 })
 export class ContactComponent implements OnInit {
   messageForm: FormGroup;
-  submitted: boolean = false;
   success: boolean = false;
+  badRequest: boolean = false;
 
   constructor(private api: ApiService, private formBuilder: FormBuilder) {
     this.messageForm = this.formBuilder.group({
       email: ["", [Validators.required, Validators.email]],
       subject: ["", Validators.required],
-      message: ["", Validators.required]
+      body: ["", Validators.required]
     });
   }
 
   onSubmit() {
-    this.submitted = true;
+    this.badRequest = false;
+    this.success = false;
 
-    if (this.messageForm.invalid) {
-      return;
-    }
-
-    this.success = true;
+    this.api
+      .postContact(this.messageForm.value)
+      .pipe(
+        catchError(() => {
+          this.badRequest = true;
+          throw new Error("This error is logged!.... or should be at least.");
+        })
+      )
+      .subscribe(contact => {
+        this.success = true;
+        // angular.element("messageModal").
+      });
   }
 
   ngOnInit() {}
